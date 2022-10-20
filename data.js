d75f2db0ddfedc757f9f60e549040f86cc0b6d45
swastikpatro <patroswastik.jsdev@gmail.com>
2022-10-20 21:56:27 +0530

    initial commit

---
+++ data.js
@@ -0,0 +1,82 @@
+const people = [
+  {
+    img: 'https://res.cloudinary.com/diqqf3eq2/image/upload/c_scale,w_200/v1595959121/person-1_aufeoq.jpg',
+    name: 'peter doe',
+    job: 'product manager',
+    text: `Lorem ipsum dolor sit amet consectetur adipisicing elit. Quidem quoeius recusandae officia voluptas sint deserunt dicta nihil nam omnis? `,
+  },
+  {
+    img: 'https://res.cloudinary.com/diqqf3eq2/image/upload/c_scale,w_200/v1595959131/person-2_ipcjws.jpg',
+    name: 'susan doe',
+    job: 'developer',
+    text: `Lorem ipsum dolor sit amet consectetur adipisicing elit. Quidem quoeius recusandae officia voluptas sint deserunt dicta nihil nam omnis?`,
+  },
+  {
+    img: 'https://res.cloudinary.com/diqqf3eq2/image/upload/c_scale,w_200/v1595959131/person-3_rxtqvi.jpg',
+    name: 'emma doe',
+    job: 'designer',
+    text: `Lorem ipsum dolor sit amet consectetur adipisicing elit. Quidem quoeius recusandae officia voluptas sint deserunt dicta nihil nam omnis?`,
+  },
+  {
+    img: 'https://randomuser.me/api/portraits/men/92.jpg',
+    name: 'kucch name 1',
+    job: 'kucch kaam 1',
+    text: `Lorem ipsum dolor sit amet consectetur adipisicing elit. Quidem quoeius recusandae officia voluptas sint deserunt dicta nihil nam omnis?`,
+  },
+  {
+    img: 'https://randomuser.me/api/portraits/women/47.jpg',
+    name: 'kucch name 2',
+    job: 'kucch kaam 2',
+    text: `Lorem ipsum dolor sit amet consectetur adipisicing elit. Quidem quoeius recusandae officia voluptas sint deserunt dicta nihil nam omnis?`,
+  },
+  {
+    img: 'https://randomuser.me/api/portraits/women/40.jpg',
+    name: 'kucch name 3',
+    job: 'kucch kaam 3',
+    text: `Lorem ipsum dolor sit amet consectetur adipisicing elit. Quidem quoeius recusandae officia voluptas sint deserunt dicta nihil nam omnis?`,
+  },
+  {
+    img: 'https://randomuser.me/api/portraits/men/42.jpg',
+    name: 'kucch name 4',
+    job: 'kucch kaam 4',
+    text: `Lorem ipsum dolor sit amet consectetur adipisicing elit. Quidem quoeius recusandae officia voluptas sint deserunt dicta nihil nam omnis?`,
+  },
+  {
+    img: 'https://randomuser.me/api/portraits/women/55.jpg',
+    name: 'kucch name 5',
+    job: 'kucch kaam 5',
+    text: `Lorem ipsum dolor sit amet consectetur adipisicing elit. Quidem quoeius recusandae officia voluptas sint deserunt dicta nihil nam omnis?`,
+  },
+  {
+    img: 'https://randomuser.me/api/portraits/men/10.jpg',
+    name: 'kucch name 6',
+    job: 'kucch kaam 6',
+    text: `Lorem ipsum dolor sit amet consectetur adipisicing elit. Quidem quoeius recusandae officia voluptas sint deserunt dicta nihil nam omnis?`,
+  },
+  {
+    img: 'https://randomuser.me/api/portraits/women/26.jpg',
+    name: 'kucch name 7',
+    job: 'kucch kaam 7',
+    text: `Lorem ipsum dolor sit amet consectetur adipisicing elit. Quidem quoeius recusandae officia voluptas sint deserunt dicta nihil nam omnis?`,
+  },
+  {
+    img: 'https://randomuser.me/api/portraits/women/61.jpg',
+    name: 'kucch name 8',
+    job: 'kucch kaam 8',
+    text: `Lorem ipsum dolor sit amet consectetur adipisicing elit. Quidem quoeius recusandae officia voluptas sint deserunt dicta nihil nam omnis?`,
+  },
+  {
+    img: 'https://randomuser.me/api/portraits/women/31.jpg',
+    name: 'kucch name 9',
+    job: 'kucch kaam 9',
+    text: `Lorem ipsum dolor sit amet consectetur adipisicing elit. Quidem quoeius recusandae officia voluptas sint deserunt dicta nihil nam omnis?`,
+  },
+  {
+    img: 'https://randomuser.me/api/portraits/women/0.jpg',
+    name: 'kucch name 10',
+    job: 'kucch kaam 10',
+    text: `Lorem ipsum dolor sit amet consectetur adipisicing elit. Quidem quoeius recusandae officia voluptas sint deserunt dicta nihil nam omnis?`,
+  },
+];
+
+export default people;
